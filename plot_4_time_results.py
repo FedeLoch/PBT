7acfb9f6fbcc480054fa01c12d9a9b51eab2cd6d
Federico Lochbaum <federico.lochbaum@gmail.com>
2026-01-14 11:19:48 +0100

    experiment paths

--- plot_4_time_results.py
+++ plot_4_time_results.py
@@ -45,10 +45,10 @@ def process_directory(directory_path, color, label):
 
 def main():
     directory_paths = [
-        '/Users/fede/Documents/Experiments/TEST-PAPER-V2/PBT/perf-muts-scripts/regexes-grammar-literals',
-        '/Users/fede/Documents/Experiments/TEST-PAPER-V2/PBT/perf-muts-scripts/regexes-grammar-derivations',
-        '/Users/fede/Documents/Experiments/TEST-PAPER-V2/PBT/perf-muts-scripts/regexes-stochastic-base',
-        '/Users/fede/Documents/Experiments/TEST-PAPER-V2/PBT/perf-muts-scripts/regexes-weighted-grammar-base'
+        '/Users/fede/Documents/PhD/2025-generating-pathological-performance-mutations/Experiment results/New experiments/Regexes/regexes-grammar-literals',
+        '/Users/fede/Documents/PhD/2025-generating-pathological-performance-mutations/Experiment results/New experiments/Regexes/regexes-grammar-derivations',
+        '/Users/fede/Documents/PhD/2025-generating-pathological-performance-mutations/Experiment results/New experiments/Regexes/regexes-stochastic-base',
+        '/Users/fede/Documents/PhD/2025-generating-pathological-performance-mutations/Experiment results/New experiments/Regexes/regexes-weighted-grammar-base'
     ]
     
     for dir_path in directory_paths:
@@ -95,6 +95,7 @@ def main():
     plt.ylabel('Microseconds (Best found as far)')
     
     plt.yscale('log')
+    plt.xscale('log')
     
     plt.legend(loc='best')
     plt.grid(True, which='both', linestyle='--', alpha=0.5)
